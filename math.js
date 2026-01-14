60f74de03987df6ff8d6e321122d34f74094883a
Ghabaei-Behzad <Ghabaei_Behzad@student.smc.edu>
2026-01-14 14:08:54 -0800

    Create math.js

---
+++ math.js
@@ -0,0 +1,22 @@
+//Behzad Ghabaei
+// math.js
+//Function 1 
+function double(n) {
+  return n * 2;
+}
+//Function 2 
+function square(n) {
+  return n * n;
+}
+//Function 3
+function isEven(n) {
+  return n % 2 === 0;
+}
+//Function 4
+function multiply(a, b) {
+  return a * b;
+}
+//Function 5
+function power(base, exponent) {
+  return Math.pow(base, exponent);
+}
