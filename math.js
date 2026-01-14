b1fa0c035e9bcce70876a829069186a03f4c9431
Ghabaei-Behzad <Ghabaei_Behzad@student.smc.edu>
2026-01-14 14:55:56 -0800

    Update math.js adding new functions such as square, isEven isOdd exponent and multipy
    Each function has a variable that is already declared so compilation of entire program requires variable declaration care.

--- math.js
+++ math.js
@@ -1,23 +1,46 @@
 //Behzad Ghabaei
 // math.js
-//Function 1 
+
+//Function 1: square a number
+let n = 4;
+function square(n) { 
+  return n * n; 
+}
+console.log("the square of " + n + " is " + square(n));
+
+//Function 2: is a number even.
+
+function isEven(n) { 
+  return n % 2 === 0; 
+}
+console.log("the number " + n + " is even: " + isEven(n));
+
+//Function 3: is the number odd
+
+function isOdd(n) { 
+  return n % 2 !== 0; 
+}
+console.log("the number " + n + " is odd: " + isOdd(n));
+
+//Function 4: multiply two numbers a, b
+let a = 3;
+let b = 4;
+function multiply(a, b) { 
+  return a * b; 
+}
+console.log("The numbers " + a + " * " + b + " = " + multiply(a,b));
+
+//Function 5: double a number
 function double(n) {
   return n * 2;
 }
 console.log("Double 5: " + double(5));
-//Function 2 
-function square(n) {
-  return n * n;
-}
-//Function 3
-function isEven(n) {
-  return n % 2 === 0;
-}
-//Function 4
-function multiply(a, b) {
-  return a * b;
-}
-//Function 5
+
+//Function 6: exponentiation
+let base = 2;
+let exponent = 3;
 function power(base, exponent) {
   return Math.pow(base, exponent);
 }
+console.log("The number " + base + " to the " + exponent + " power is: " + power(base, exponent));
+
